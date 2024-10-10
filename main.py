21455d0ce695d2d41165da50e5f189f72d714b95
Nash Pillai <nash.pillai@protonmail.com>
2024-10-10 15:40:43 -0400

    change syntax to spike prime

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.ev3devices import GyroSensor
-from pybricks.parameters import Port, Direction
+from hub import light_matrix
+from hub import motion_sensor
+from hub import port
+import runloop
+import motor
+
 
 import math
 
@@ -12,14 +13,12 @@ import time
 # sys.stdout = open('logs.txt','at')
 # print()
 
-ev3 = EV3Brick()
-
-right_motor = Motor(Port.D)
-left_motor = Motor(Port.A)
+right_motor = port.B
+left_motor = port.A
 motors = [left_motor, right_motor]
 
-gyro = GyroSensor(Port.S4, Direction.COUNTERCLOCKWISE)
-gyro.reset_angle(0)
+gyro = motion_sensor
+gyro.reset_yaw(gyro.tilt_angles()[0])
 
 heading_error = 0
 last_heading_error = 0
@@ -53,8 +52,8 @@ degrees_per_tile = 360 * 2.85
 
 current_pos = (0.5, 0)
 
-def main():
-	ev3.screen.draw_text(0, 0, "Starting!")
+async def main():
+	print(0, 0, "Starting!")
 
 	plan_path([{"x": current_pos[0], "y": current_pos[1]},
 		{"x": 0.5, "y": 0.5}, # Move onto Field
@@ -72,7 +71,7 @@ def main():
 		{"x": 0.5, "y": 3.5}, # End
 	])
 
-	ev3.screen.draw_text(0, 50, "Done! -- " + str(time_elapsed()))
+	print(0, 50, "Done! -- " + str(time_elapsed()))
 	print("Done!\n Time taken:", time.time() - start_time, "seconds (target:", target_time, "seconds)")
 	time.sleep(10)
 
@@ -93,7 +92,7 @@ def angle_closest_dir(initial, target):
 	diff = target - initial
 	return sorted([diff, diff + math.copysign(360, -diff)], key=abs)[0]
 
-def get_angle(): return gyro.angle() % 360
+def get_angle(): return gyro.tilt_angles()[0] % 360
 
 
 def turn_to(target_angle, speed, tol):
@@ -102,18 +101,18 @@ def turn_to(target_angle, speed, tol):
 
 	print("Currently:", str(get_angle()), "deg and turning to:", str(target_angle), "deg")
 
-	while not abs(angle_closest_dir(get_angle(), target_angle)) < tol: 
+	while not abs(angle_closest_dir(get_angle(), target_angle)) < tol:
 		turning_error = angle_closest_dir(get_angle(), target_angle)
 		turning_speed = turning_pid["kp"] * turning_error + turning_pid["ki"] * (turning_error + last_turning_error) + turning_pid["kd"] * (turning_error - last_turning_error)
 		turning_speed = clamp(-400, turning_speed, 400)
 
-		left_motor.run(turning_speed * speed)
-		right_motor.run(-turning_speed * speed)
+		motor.run(left_motor, turning_speed * speed)
+		motor.run(right_motor, -turning_speed * speed)
 
 		last_turning_error = turning_error
 
-	left_motor.hold()
-	right_motor.hold()
+	motor.stop(left_motor, stop=motor.HOLD)
+	motor.stop(right_motor, stop=motor.HOLD)
 	remaining_turns -= 1
 	print("Finished Turning, final angle is:", str(get_angle()), "deg")
 
@@ -122,28 +121,28 @@ def drive_straight(distance, speed, target_angle=None):
 	global heading_error, last_heading_error, heading_pid
 	target_angle = target_angle if (target_angle is not None) else get_angle()
 
-	right_motor.reset_angle(0)
-	left_motor.reset_angle(0)
+	motor.reset_relative_position(right_motor, 0)
+	motor.reset_relative_position(left_motor, 0)
 
-	while abs(left_motor.angle()) < abs(distance * degrees_per_tile): 
+	while abs(motor.relative_position(left_motor)) < abs(distance * degrees_per_tile):
 		heading_error = angle_closest_dir(get_angle(), target_angle)
 		turning_speed = heading_pid["kp"] * heading_error + heading_pid["ki"] * (heading_error + last_heading_error) + heading_pid["kd"] * (heading_error - last_heading_error)
 		turning_speed = clamp(-500, turning_speed, 500)
 
-		left_motor.run(-speed * (1 if distance < 0 else -1) + turning_speed)
-		right_motor.run(-speed * (1 if distance < 0 else -1) - turning_speed)
+		motor.run(left_motor, -speed * (1 if distance < 0 else -1) + turning_speed)
+		motor.run(right_motor, -speed * (1 if distance < 0 else -1) - turning_speed)
 
 		last_heading_error = heading_error
 
-	left_motor.hold()
-	right_motor.hold()
+	motor.stop(left_motor, stop=motor.HOLD)
+	motor.stop(right_motor, stop=motor.HOLD)
 
 def move_to(x: int, y: int, run_backwards=False, update_pos=True, angle_offset=0, distance_offset=0):
 	global current_pos, remaining_distance
 
 	if (current_pos == (x, y)):
 		return
-	
+
 	target_angle = math.atan2(y - current_pos[1], x - current_pos[0]) * 360 / (2 * math.pi) - 90 + angle_offset
 	if run_backwards:
 		target_angle += 180
@@ -156,7 +155,7 @@ def move_to(x: int, y: int, run_backwards=False, update_pos=True, angle_offset=0
 
 	drive_straight(distance, required_angular_speed(), target_angle)
 
-	if update_pos: 
+	if update_pos:
 		current_pos = (x, y)
 		remaining_distance -= abs(distance - distance_offset)
 
@@ -166,11 +165,12 @@ def plan_path(segments: list[dict]):
 	remaining_distance = 0
 	for i in range(1, len(segments)):
 		remaining_distance += math.sqrt((segments[i]["x"] - segments[i - 1]["x"])**2 + (segments[i]["y"] - segments[i - 1]["y"])**2)
-	
+
 	# remaining_turns = len(list(filter(lambda a: a.get("run_backwards") != True, segments[1:])))
 	remaining_turns = len(segments) - 1
 	print("Starting at", current_pos, "with", remaining_distance, "tiles to go")
 	current_pos = (segments[0]["x"], segments[0]["y"])
 	for s in segments[1:]: move_to(**s)
 
-if __name__ == '__main__': main()
+
+runloop.run(main())
