89c26d7cb34734f5c8cc92ac2edcb02acb133815
matthewglasser <matthewsglasser@gmail.com>
2025-11-23 20:43:30 -0500

    25-26

--- main.py
+++ main.py
@@ -38,20 +38,20 @@ turning_pid = {
 }
 
 start_time = time.time()
-target_time = 58 - .4
+target_time = 64
 
 # mm/s
 slow_speed = 100
 fast_speed = 1200
 
-turn_speed = 1
+turn_speed = .5
 
 remaining_distance = 0
 remaining_turns = 0
 
-degrees_per_tile = 360 * 2.85
+degrees_per_tile = 360 * 2.42
 
-current_pos = (3, 0.5)
+current_pos = (2, 0.5)
 
 def main():
 	ev3.screen.draw_text(0, 0, "Starting!")
@@ -60,17 +60,21 @@ def main():
 	path = [
 		# Initial Position
 		{"x": current_pos[0], "y": current_pos[1]},
-		{"x": 3, "y": 2},
-		{"x": 1, "y": 2},
-		{"x": 2.5, "y": 2},
-		{"x": 3.5, "y": 1},
-		{"x": 4.5, "y": 2},
-		{"x": 5, "y": 1.3},
-		{"x": 5, "y": 4},
-		{"x": 4, "y": 4},
-		{"x": 3.5, "y": 3},
-		{"x": 2.5, "y": 4},
-		{"x": 2, "y": 4},
+		{"x": 2, "y": 1},
+  	{"x": 3.5, "y": 1},
+    {"x": 4.5, "y": 2},
+    {"x": 5, "y": 1.3}, #B
+    {"x": 5, "y": 2},
+    {"x": 4, "y": 2},
+    {"x": 4, "y": 3.5},
+    {"x": 4.8, "y": 4}, #D
+    {"x": 3.5, "y": 3, "run_backwards": True},
+    {"x": 1, "y": 3},
+    {"x": 1, "y": 2.3}, #C
+    {"x": 1.5, "y": 3, "run_backwards": True},
+    {"x": 2.8, "y": 4}, #A
+    {"x": 1, "y": 3, "run_backwards": True},
+    {"x": 1, "y": 4},
 	]
 	path = list(map(lambda point: dict(list(point.items()) + [["x", point["x"] - 0.5], ["y", point["y"] - 0.5]]), path))
 	plan_path(path)
